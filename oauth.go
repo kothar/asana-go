444a61e96c23dd9e1c74c27d60cc32b7fb2b29f1
Mike Houston <mike@kothar.net>
2017-07-09 23:48:16 +0100

    Added OAuth handling

---
+++ oauth.go
@@ -0,0 +1,56 @@
+package asana
+
+import (
+	"context"
+
+	"golang.org/x/oauth2"
+)
+
+var defaultOAuthEndpoint = oauth2.Endpoint{
+	AuthURL:  "https://app.asana.com/-/oauth_authorize",
+	TokenURL: "https://app.asana.com/-/oauth_token",
+}
+
+// AppConfig provides the details needed to authenticate users with
+// Asana on behalf of an Asana client application
+type AppConfig struct {
+	ClientID     string
+	ClientSecret string
+	RedirectURL  string
+}
+
+// App represents an Asana client application
+type App struct {
+	config *oauth2.Config
+}
+
+// NewApp creates a new App with the provided configuration
+func NewApp(config *AppConfig) *App {
+	return &App{
+		config: &oauth2.Config{
+			ClientID:     config.ClientID,
+			ClientSecret: config.ClientSecret,
+			Endpoint:     defaultOAuthEndpoint,
+			RedirectURL:  config.RedirectURL,
+			Scopes:       nil,
+		},
+	}
+}
+
+// see oauth2 package
+func (a *App) AuthCodeURL(state string) string {
+	return a.config.AuthCodeURL(state)
+}
+
+// see oauth2 package
+func (a *App) Exchange(code string) (*oauth2.Token, error) {
+	ctx := context.Background()
+	return a.config.Exchange(ctx, code)
+}
+
+// NewClient creates a new Asana client using the provided credentials
+func (a *App) NewClient(token *oauth2.Token) *Client {
+	ctx := context.Background()
+	client := a.config.Client(ctx, token)
+	return NewClient(client)
+}
